8c07086b1ca31459a301f2e55eef5f6c00269925
sergio <sergiocanalesm1@gmail.com>
2020-11-20 19:18:33 -0500

    concurrent tasks synchronized

--- bin/untitled.dart
+++ bin/untitled.dart
@@ -4,8 +4,11 @@ import 'package:rxdart/rxdart.dart';
 import 'dart:async';
 import 'dart:math';
 
-class QueueHelper{
 
+
+class QueueHelper{
+  static Future<Null> isWorking;
+  static Completer<Null> completer;
   static Queue<int> queue = Queue<int>();
   static final StreamController<int> _controller = BehaviorSubject<int>();
 
@@ -15,6 +18,17 @@ class QueueHelper{
     for(var i = 0; i < 10; i++){
       queue.add( i+1 );
     }
+  }
+
+  static Future<void> administrador() async {
+    if ( isWorking != null ){
+      print("waiting");
+      await isWorking;
+      return administrador();
+    }
+    completer = Completer<Null>();
+    isWorking =  completer.future;
+    await ventana();
   }
 
   static Stream<int> carpintero( { chance = 10 } ) async* {
@@ -28,24 +42,28 @@ class QueueHelper{
       yield await Future.delayed( Duration( seconds: 1 ), () => current );
       QueueHelper.queue.removeFirst();
     }
+    completer.complete();
+    isWorking = null;
   }
 
-  static void administrador_carpinteria(){
+  static void ventana() async {
     if( QueueHelper.queue.isNotEmpty ){
-      carpintero().handleError(( error ){
+        carpintero().handleError(( error ){
         print( "erroooooor capturado en stream 1, ${ error.toString() }");
-        Future.delayed( Duration( seconds: 1 ), () => administrador_carpinteria() );
+        Future.delayed( Duration( seconds: 1 ), () => ventana() );
       })
       .listen(( event ){
         _controller.sink.add( event );
-      }, cancelOnError: false, )
-      ;
+      }, cancelOnError: false, );
+
     }
   }
+
 }
 
-void main( List<String> arguments ) async {
+void main( List<String> arguments ) {
   QueueHelper.fillQueue();
+
   QueueHelper.stream()
     .where(( event ) => event % 2 == 0 )
     .map(( event ) => "es par $event" )
@@ -59,7 +77,11 @@ void main( List<String> arguments ) async {
     .map(( event ) => "es impar $event" )
     .listen( print );
 
-  QueueHelper.administrador_carpinteria();
-  //QueueHelper.administrador_carpinteria();
+
+
+  QueueHelper.administrador();
+  QueueHelper.administrador();
+  QueueHelper.administrador();
+   //QueueHelper.administrador_carpinteria();
   //validar que no se ejecute si ya hay una en ejecucion -- encontrar forma de hacer el analogo a synchronize de java
 }
